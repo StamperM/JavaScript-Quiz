7a7e2e17bb820648a14e698df3fba14aac2e130e
melissa <90412072+StamperM@users.noreply.github.com>
2023-01-06 19:45:11 -0500

    added the clear leaderboard function

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -40,17 +40,17 @@ var questions = [
 ];
 var currentIndex = 0;
 
-// score will count down from 60 seconds
+// startQuiz will start the timer countdown,  will hide the start page, and show the first question
 
 function startQuiz() {
   scoreboardFun();
   document.getElementById("startQuizSection").style.display = "none";
   document.getElementById("quizQuestionContainer").style.display = "block";
   displayQuestion(currentIndex);
-  console.log(scoreboardFun);
+  
 }
 
-// displayQuestion () will display the question of the index it is at.
+// displayQuestion () will populate and display the question of the index it is at. Calling the answerTheQuestions() 
 function displayQuestion(index) {
   console.log("hello");
   question1Element.textContent = questions[index].title;
@@ -59,7 +59,6 @@ function displayQuestion(index) {
   answerTwo.textContent = questions[index].answers[1];
   answerThree.textContent = questions[index].answers[2];
   answerFour.textContent = questions[index].answers[3];
-  console.log(document.querySelectorAll(".userAnswer"));
   document.querySelectorAll(".userAnswer").forEach((answer) => {
     answer.addEventListener("click", (event) => {
       userInputAnswer = event.target.textContent;
@@ -78,16 +77,18 @@ function answerTheQuestions() {
     score -= 10;
     displayNextQuestion();
   }
-  // return endOfGame();
 }
+// displayNext Question will cont to display questions until it reaches the greater than the questions array length.
 function displayNextQuestion() {
   currentIndex++;
   if (currentIndex < questions.length) {
     displayQuestion(currentIndex);
+  } else {
+    return endOfGame();
   }
 }
 
-//
+// runTheScore down says that if score is greater than zero it will log the score/timer down by one. Once the score = 0 the game will end.
 
 function runTheScore() {
   if (score >= 0) {
@@ -98,38 +99,58 @@ function runTheScore() {
     clearInterval(scoreboardFun);
   }
 }
+
+// This function is to uses setInterval method to have our time count down my 1 every second.
 function scoreboardFun() {
   let scoreboard = setInterval(runTheScore, setTimer);
-  endOfGame();
 }
-
 //  ToDo: For All Done unhide #lastSection and Hide #quizQuestions Container user score will be pulled from the score element and be written to totalScore. userInitals will need to be a user input and stored to local storage.
 function endOfGame() {
   document.getElementById("quizQuestionContainer").style.display = "none";
   document.getElementById("lastSection").style.display = "block";
-  document.getElementById('allDone').textContent = "All Done"
-  document.getElementById("totalScore").textContent = `Your final score is ${score}`;
+  document.getElementById("allDone").textContent = "All Done";
+  document.getElementById(
+    "totalScore"
+  ).textContent = `Your final score is ${score}`;
 
- 
- saveScore();
- 
+  saveScores();
 }
-function saveScore(){
-  var leaderBoardInfo ={
-    Player: document.getElementById('userinputInitals'),
-    playerScore: score
-  };
-  localStorage.setItem("name","userinputIntials"),JSON.stringify(leaderBoardInfo);
-}
- 
 
+function saveScores() {
+  var leaderBoardString = window.localStorage.getItem("leaderBoardInfo");
+  var scores = [];
+  if (leaderBoardString) {
+    scores = JSON.parse(leaderBoardString);
+  }
+  scores.push({
+    Player: document.getElementById("userinputInitals").value,
+    playerScore: score,
+  });
+  localStorage.setItem("leaderBoardInfo", JSON.stringify(scores));
+}
 
 // Todo: High Scores Unhide Leaderboard &hide Last section. Create <li> to store the userIntials and Score scores in #leaderBoardList.
-function displayScoresOnBoard(){
-  var recentScore = JSON.parse(localStorage.getItem('leaderBoardInfo'))
-
-
+function displayScoresOnBoard() {
+  var scores = JSON.parse(localStorage.getItem("leaderBoardInfo"));
+  if (scores) {
+    var sortedscores = scores.sort((a, b) => {
+      a.playerScore > b.playerScore;
+    });
+    sortedscores.forEach((score) => {
+      score.Player;
+      score.playerScore;
+    });
+    sortedscores.forEach((item) => {
+      let li = document.createElement("li");
+      li.innerText = item;
+      list.appendChild(li);
+    });
+  }
 }
 // Todo: Return user to start quiz #returnToQuiz
 
 // Todo: clear the scores off the board. May need to look at how to clear local storage?
+function clearLeaderBoard(){
+  var leaderList = document.getElementById("leaderBoardList")
+  leaderList = "";
+}
